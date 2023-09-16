ecda1abf78281b2c81000b9209a53b34c9d48fea
kreshan <kreshan882@gmail.com>
2023-09-16 20:14:20 +0800

    commit 1AS

--- SpringLearn_JPA/src/main/java/com/kre/springjpa/repositery/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kre.springjpa.repositery;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kre.springjpa.entity.Teacher;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Long>{
-
-}
